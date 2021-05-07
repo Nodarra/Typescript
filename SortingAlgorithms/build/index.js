c8521d23e8ba1d3d73d852b6252e73638a1312a4
Nodarra <sisicdino4@gmail.com>
2021-05-07 22:14:58 +0200

    Universal sorting for all data structures

---
+++ SortingAlgorithms/build/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var CharactersCollection_1 = require("./CharactersCollection");
+/* const numberCollection = new NumbersCollection([-7, 50, 5, 0, -5])
+numberCollection.sort();
+console.log(numberCollection.data) */
+var charactersCollection = new CharactersCollection_1.CharactersCollection('ananas');
+charactersCollection.sort();
+console.log(charactersCollection.data);
+/* const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+linkedList.sort();
+linkedList.print(); */ 
